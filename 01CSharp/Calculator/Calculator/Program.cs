7764dec53233ff03ad36df44dc762f79c4a1e405
Jameson Dy <dyjameson22@gmail.com>
2020-01-08 15:15:15 -0600

    Added Reverse_FizzBuzz program

--- 01CSharp/Calculator/Calculator/Program.cs
+++ 01CSharp/Calculator/Calculator/Program.cs
@@ -6,10 +6,30 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            /*
             Mathematics m = new Mathematics();
             Console.WriteLine("Substraction: " + m.Subs(5, 6));
             Console.WriteLine("Addition: " + m.Add(5, 6, 4));
             Console.WriteLine("Division: " + m.Divide(5, 2));
+            */
+
+            int[] nums;
+            nums = new int[10];
+            Console.WriteLine("Enter 10 numbers");
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Display");
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
         }
+
+
     }
 }
---
+++ 01CSharp/Reverse_FizzBuzz/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.WriteLine("Write string to reverse: ");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine(Reverse(userInput) + "\n");
+
+            int userInputInt = 0;
+            do
+            {
+                Console.Write("Write FizzBuzz number (0) to quit: ");
+                userInputInt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(FizzBuzz(userInputInt));
+            }
+            while (userInputInt != 0);
+
+        }
+
+        // write program to reverse string w/o using reverse method
+        public static string Reverse(string s)
+        {
+            string result = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                result += s[i];
+            }
+            return result;
+        }
+
+        // write a fizzbuzz game
+        public static string FizzBuzz(int n)
+        {
+            if ((n % 3 == 0) && (n % 5 == 0))
+            {
+                return "FizzBuzz";
+            } else if (n % 3 == 0)
+            {
+                return "Fizz";
+            } else if (n % 5 == 0)
+            {
+                return "Buzz";
+            } else
+            {
+                return n.ToString();
+            }
+        }
+    }
+}
